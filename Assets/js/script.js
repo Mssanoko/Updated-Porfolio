18a77d1cc756745c039c0a08e770b87aadee0477
Leslie Lawson <lawsonjleslie@gmail.com>
2020-09-22 22:17:34 -0400

    progressive bar

---
+++ Assets/js/script.js
@@ -0,0 +1,11 @@
+// Progress Bar
+jQuery(function ($) {
+    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+});
